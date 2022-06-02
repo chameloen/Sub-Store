213289513ea8c5b1eff59a21852b6f4d1b648789
Hsiaoyi Hsu <xreamxu@gmail.com>
2022-06-02 14:40:01 +0800

    Deploy Frontend

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.PUBLIC_PATH || (process.env.NODE_ENV === 'production'
+    ? '/Sub-Store/'
+    : '/'),
   "transpileDependencies": [
     "vuetify",
     'vue-echarts',
